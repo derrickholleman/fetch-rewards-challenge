bd549882da17619ffb286dc22a3cd138cbfeb553
Derrick Holleman <derrickholleman@gmail.com>
2022-01-12 10:26:15 -0600

    form fields created

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,12 +1,47 @@
-import React from 'react'
-import './Form.css'
+import React from "react";
+import "./Form.css";
 
 const Form = () => {
-    return (
-        <div>
-            
+  return (
+    <div className="form-wrapper">
+      <form>
+        <div className="form-field">
+          <label htmlFor="full-name">
+            Full Name:<span className="red-accent">*</span>
+          </label>
+          <input type="text" id="full-name" required />
         </div>
-    )
-}
 
-export default Form
+        <div className="form-field">
+          <label htmlFor="email">
+            Email:<span className="red-accent">*</span>
+          </label>
+          <input type="text" id="email" required />
+        </div>
+
+        <div className="form-field">
+          <label htmlFor="password">
+            Password:<span className="red-accent">*</span>
+          </label>
+          <input type="password" id="password" required />
+        </div>
+
+        <div className="form-field">
+          <label htmlFor="occupation">
+            Occupation:<span className="red-accent">*</span>
+          </label>
+          <input type="text" id="occupation" required />
+        </div>
+
+        <div className="form-field">
+          <label htmlFor="state">
+            State:<span className="red-accent">*</span>
+          </label>
+          <input type="text" id="state" required />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
