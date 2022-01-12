77c1ff730f1092094790ed261d2b750ca350eca1
Derrick Holleman <derrickholleman@gmail.com>
2022-01-12 15:08:57 -0600

    success message edit

--- src/Success.jsx
+++ src/Success.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Success = () => {
   return (
     <div className="centering-wrapper">
-      <h1>Form successfully submitted!</h1>
+      <h2>Form successfully submitted!</h2>
       <Link to="/">Submit another form?</Link>
     </div>
   );
